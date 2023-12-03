fa0d48e98db94ba2d0b5442eb13eace7d5869110
Archit <bhatnagar_archit@yahoo.co.in>
2023-12-03 13:30:00 +0100

    Problem 03 Part 2

--- 03/main.py
+++ 03/main.py
@@ -28,7 +28,6 @@ def get_number_adjacent_elements(
     This function does not perform sanity checks on the indices -- if the indices are out-of-bounds
     or if the adjacent elements will be out-of-bounds, the output is non-sensical.
     """
-    # Row and column indices of original input_matrix are now shifted by one
     row_above = input_matrix[row_ind - 1][col_start - 1 : col_end + 2]
     row_below = input_matrix[row_ind + 1][col_start - 1 : col_end + 2]
     col_left = input_matrix[row_ind][col_start - 1]
@@ -74,6 +73,48 @@ def get_part_numbers(input_matrix: list[str]) -> list[int]:
     return part_numbers
 
 
+def get_gear_ratio(row_ind: int, col_ind: int, input_matrix: list[str]) -> str:
+    """
+    Finds the gear ratio of the gear '*' at a given location (row_ind,col_ind) in input_matrix.
+
+    Gear ratio is defined as the product of the part numbers adjacent to the gear. A gear is
+    a '*' with only two part numbers adjacent to it. This function returns 0 if the gear ratio
+    is not defined -- that is, if the number of part numbers adjacent to the given location of
+    '*' is not exactly two.
+
+    This function does not perform sanity checks on the indices -- if the indices are out-of-bounds
+    or if the adjacent elements will be out-of-bounds, the output is non-sensical.
+    """
+    numbers_adjacent = []
+    for line in input_matrix[(row_ind - 1) : (row_ind + 2)]:
+        # Get numbers in row
+        for num in re.finditer(r"\d+", line):
+            # Check if adjacent to given location
+            col_start, col_end = num.start(), num.end() - 1
+            if (col_start - 1) <= col_ind <= (col_end + 1):
+                numbers_adjacent.append(int(num.group(0)))
+    # Check if gear -- exactly two adjacent part numbers
+    if len(numbers_adjacent) == 2:
+        return numbers_adjacent[0] * numbers_adjacent[1]
+    else:
+        return 0
+
+
+def get_gear_ratios(input_matrix: list[str]) -> list[int]:
+    """
+    Finds all gear ratios in given input_matrix. Looks for '*' in input_matrix and calculates gear
+    ratio for each '*'. If '*' has not exactly 2 adjacent part numbers, gear ratio is set to 0.
+    """
+    gear_ratios = []
+    # Create padded matrix for get adjacent elements -- 1 row and 1 col padding
+    padded_matrix = pad_matrix(input_matrix)
+    # Find gears
+    for ind_row, line in enumerate(padded_matrix):
+        for gear in re.finditer(r"\*", line):
+            gear_ratios.append(get_gear_ratio(ind_row, gear.start(), padded_matrix))
+    return gear_ratios
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="AoC 2023 Problem 3")
     parser.add_argument("input_file", help="Input file")
@@ -81,3 +122,4 @@ if __name__ == "__main__":
 
     user_input_matrix = read_input_file(Path(args.input_file))
     print(f"Sum of part numbers is: {sum(get_part_numbers(user_input_matrix))}")
+    print(f"Sum of gear ratios is: {sum(get_gear_ratios(user_input_matrix))}")
